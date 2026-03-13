7d7cb1fa0e5b31d8e87bb62e1f2cdbc458eaadcb
Tristanlimyr <87318834+Tristanlimyr@users.noreply.github.com>
2026-03-13 15:52:16 +0800

    Edit code to comply with coding standard

--- src/main/java/seedu/address/model/itinerary/exceptions/DuplicateItineraryException.java
+++ src/main/java/seedu/address/model/itinerary/exceptions/DuplicateItineraryException.java
@@ -1,8 +1,8 @@
 package seedu.address.model.itinerary.exceptions;
 
 /**
- * Signals that the operation will result in duplicate Itineraries (Itineraries are considered duplicates if they have the same
- * name).
+ * Signals that the operation will result in duplicate Itineraries (Itineraries are considered duplicates if they have
+ * the same name).
  */
 public class DuplicateItineraryException extends RuntimeException {
     public DuplicateItineraryException() {
