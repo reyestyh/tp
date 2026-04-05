a030ca860a404fcf42a01174ecd1931583c963a3
Shaine Gan <176516998+shainegan1407@users.noreply.github.com>
2026-04-06 00:07:42 +0800

    Standardise error messages

--- src/main/java/seedu/address/logic/Messages.java
+++ src/main/java/seedu/address/logic/Messages.java
@@ -16,9 +16,11 @@ public class Messages {
     public static final String MESSAGE_UNKNOWN_COMMAND = "Unknown command";
     public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Invalid command format! \n%1$s";
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX =
-            "Invalid index: The person index provided is invalid";
+            "Invalid index: Unable to find the contact at the specified index. "
+                    + "Please ensure the contact list is displayed and use a valid index shown in the list.";
     public static final String MESSAGE_INVALID_ITINERARY_DISPLAYED_INDEX =
-            "Invalid index: The itinerary index provided is invalid";
+            "Invalid index: Unable to find the itinerary at the specified index. "
+                    + "Please ensure the itinerary list is displayed and use a valid index shown in the list.";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
     public static final String MESSAGE_DUPLICATE_FIELDS =
                 "Duplicate fields: Multiple values specified for the following field(s): ";
